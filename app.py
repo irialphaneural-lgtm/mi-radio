ef99ad1e8deaff013f48c338636de6ec351bc184
irialphaneural-lgtm <iri.alphaneural@gmail.com>
2026-07-11 01:16:29 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,14 @@
-"
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    # El widget oficial de Zeno aprobado para incrustar externamente
+    widget_url = "https://zeno.fm"
+    
+    # Enlaces oficiales directos de tus redes sociales
+    url_facebook = "https://facebook.com"
     url_youtube = "https://youtube.com"
 
     return f'''

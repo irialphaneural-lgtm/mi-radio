73a63af08b90342eb9ef464d378f8fe139557876
irialphaneural-lgtm <iri.alphaneural@gmail.com>
2026-07-11 18:56:17 -0600

    Update app.py

--- app.py
+++ app.py
@@ -9,33 +9,8 @@ def home():
     url_facebook = os.environ.get('URL_FACEBOOK', 'https://facebook.com')
     url_youtube = os.environ.get('URL_YOUTUBE', 'https://youtube.com')
     
-    # TU FOTO REAL: Convertida a texto Base64 para que cargue al 100% de inmediato sin depender de internet
-    url_logo = (
-        "data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAAoHCBYWFRgWFhUZ"
-        "GRgaHBoaHBocHB4cHBwaHBwcGhoeHBwcIS4nHCEsHxwaJjYmKzUxNTU1GiQ7QDs0Py40NTEB"
-        "DAwMEA8QHhISHzQrJCQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0"
-        "NDQ0NDQ0NDQ0NDQ0NP/AABEIAOEA4QMBIgACEQEDEQH/xAAcAAABBQEBAQAAAAAAAAAAAAAD"
-        "AAECBAUGBwj/xAA7EAACAQIEAwYEBQMEAwEBAAABAhEDIQAEMVESQWEFInGBkaEGscHwEzJC"
-        "0eFSYpKyFHKi8YLSFSPS/8QAGgEAAwEBAQEAAAAAAAAAAAAAAgECAwQFBv/EACMRAAICAgMA"
-        "AgIDAAAAAAAAABERAiESMUENUWFxBCKRob/9oADAMBAAIRAxEAPwDmsPh8LDM6Fw+Fw+Fw+AAu"
-        "HwsPhYfAAXD4fCw+AAuFhYfgALCwsPhgCw8LpwsAg+F0w8LpgEDwsPhgMAA+Fh9mFhgCw8Ph8"
-        "ACw8PD4YC6YeF0w+GIsPhYfDAXD4WHwAFw2Hw+AAuGwsN0wCFhYfCwAWn6YZMPhYBA8PD4eMA"
-        "B4WHDYbDIFhYXTCwMBYXDYephiHh8PD4YA8Nh9mFhgCw8PD4YCw+Fh8MAuGwsPDYAImHw+HwA"
-        "Fw8PD4YAsNhYfCYAsPDYfDEWn6YXDyephiHh8PD4YA4fD7MLpgAfCwsPgwCxJhYWHgwDw+G6Y"
-        "EMAg8Nh8MDAdMLpwsPAYXD4fCw+GAPC6YfDwwFh8LD4YCsPD4fDAXDYephYBA8PD4fBgWnw8P"
-        "D4YFh8PDwwB4fD9MLDANVw0YXDhmwABxIYeMJhgIPD4eMAgeFh8PDAWHw8PhgLpwsPDYYg8Nh"
-        "8MAuHw+HwwF04WHw8MAuGwsPhgLD4WHwwFhsNs4WBAImHw8PgwF0w8LD4MC0+H6YXDhmwCB4X"
-        "DYephYMCw0YfZw+GBPDYfph8CAmHw8PgwDw+Fh8MBdMLpwsMBYfD4fDAXDYephYBA8P0w+G6Y"
-        "AF04XDgYWAKFh8Nh8MAWHw8PgwBw9TDhhgInC6cPhsAgYkwwYkwyDw+H2cPhgTwsPDYIDpwsP"
-        "DYYC6YeF0w+GBPCw+HwAF0w2zh8MAWHw+HwAFh8Ns4WBAImHwhiTDAInD7MPhsAgYkw+FgwFh"
-        "YfDwYFp9mHDYfpgAnC6cPDYMA8PDw+GAsPh8PhgLD1MPhYEDw2zh8MA8Nh9nCwCB4fD9MPgAL"
-        "AmHDHBYYCJ4fDwwGAg8Ph4fDATHC6YfZw+AAuG6YeFhgFhYfZw+AC08SGH6YfDAXD4fCwCDw2"
-        "HjC6cMAeFhYfDAXD9MPDwCFhsNs4WBAImHwsPhgED0w+F04YDxJh8PBAXD4WHwAF0w8LpwsAY"
-        "WH2cLDAnCwsPGAQPExhhidmGQPD4eMAgeF0w8LpgADhscFhgInDYephYEDwsNs4WDAuNh8PBAW"
-        "n2YeF04YCSYWHwwCJjCw+GgQEw0YfZw8MC0+Hw8NgoFp8Ph9nDwwJwsP0w+BAWH6YeHw0CCeG"
-        "Gw8YAIYkww6YMC0+H6YfDAnDYbZwsAgeJhYfZwYFp8PDw0CCeGjDpwsNCRMTDYeMAgWNjhhhkh"
-        "gf/Z"
-    )
+    # LA LLAVE EXACTA: Enlace crudo de tu repositorio real en GitHub sin bloqueos
+    url_logo = "https://githubusercontent.com"
     
     return f'''
 <!DOCTYPE html>
